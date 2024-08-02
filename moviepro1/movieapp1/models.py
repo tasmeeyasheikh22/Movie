ec7265c02f073589a50701216a49fe13ee38ec4b
abc <tasmeeyasheikh2001@gmail.com>
2024-08-02 15:57:41 +0530

    Final commit

--- moviepro1/movieapp1/models.py
+++ moviepro1/movieapp1/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
 class Movie(models.Model):
     id = models.IntegerField(primary_key=True)
-    movieTitle = models.CharField(max_length=39)
+    movieTitle = models.CharField(max_length=39,validators=[validators.MaxLengthValidator(9)])
     director = models.CharField(max_length=40)
     review_content = models.CharField(max_length=35)
     rating = models.IntegerField()
--- moviepro1/movieapp1/views.py
+++ moviepro1/movieapp1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import MovieForm
-from .models import models
+from .models import Movie
 
 # Create your views here.
 def addview(request):
@@ -13,19 +13,20 @@ def addview(request):
     return render(request,"movieapp1/add.html",{"form":form})
 
 def viewsview(request):
-    obj = MovieForm.objects.all()
+    obj = Movie.objects.all()
     return render(request,"movieapp1/views.html",{"mov":obj})
 
 def updateview(request,id):
-    obj= MovieForm.objects.get(id=id)
+    obj= Movie.objects.get(id=id)
     form = MovieForm(instance=obj)
     if request.method=="POST":
         form = MovieForm(request.POST,instance=obj)
         if form.is_valid():
             form.save()
-            return redirect("/a1/av/")
-    return render(request,"movieapp1/add.html",{})
+            return redirect("/a1/sv/")
+    return render(request,"movieapp1/add.html",{"form":form})
 
 def deleteview(request,ud):
-    obj = MovieForm.objects.get(id=ud)
+    obj = Movie.objects.get(id=ud)
     obj.delete()
+    return redirect('/a1/sv/')
